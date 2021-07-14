2e5b2b67a90a82393c097223433304a33bdca4f7
santagatalab <44001783+santagatalab@users.noreply.github.com>
2021-07-14 16:51:34 -0400

    Update RUN_ALL.m

--- KP_Mouse_Vaccine/IMAGE ANALYSIS CODES/RUN_ALL.m
+++ KP_Mouse_Vaccine/IMAGE ANALYSIS CODES/RUN_ALL.m
@@ -1,58 +1,25 @@
 clear all
 %
 % t-CycIF ASHLAR ANALYSIS PIPELINE 
-% Step 0: Stitch and register the fields into an "ome.tif" by using Ashlar 
-% Step 1:(RUN_Step1_new_fields_preilastik_crops.m) Cut the "ome.tif" into 
-% fields of a size specified by the user and cut crops out of each field 
+% Before starting: Stitch and register the fields into an "ome.tif" by using ASHLAR (https://doi.org/10.1101/2021.04.20.440625)
+% Step 1:(Step1_fields_preilastik.m) Cuts the "ome.tif" into fields of a size specified by the user and cut crops out of each field 
 % for Ilastik training. Create full stacks of all cycles and channels. 
-% Omits fields that have no cells.   
-% Step 2: Create segmentation probabilities using Ilastik
-% (RUN_ALL_segon.m)Runs segmentation and measurements scripts (Step 3 & 4) 
-% Step 3:(RUN_Step3_segmentfromilastik.m) Segment based on segmentation
-% probabilities produce by Ilastik
-% Step 4: (RUN_Step4_CycIF_measurments_ilastik.m) Makes measurements of
-% signal and foci segmentation 
-% Step 5: (RUN_Step5_ROI.m) Finds pixel location of ROIs and indexes of
-% centroids within the ROI 
+% Step 2: (Step2b_filtercrops_v2.m) OPTIONAL Omits fields that have no cells. 
+% Please note that if the stacks produced by Matlab are not recognized as multichannel image % the addition 
+% RUN_Step2_changetifffromZtoXhannel_yescrops_O2.ijm needs to be run in Fiji/ImageJ (https://imagej.net/software/fiji/)
+% Step 2: (needs to be run externally through Ilastik https://www.ilastik.org/) Create segmentation probabilities using Ilastik
+% Step 3: (Step3_segmentfromilastik_v2_LabelAndDilate.m) Segment based on probabilities produced by Ilastik
+% Step 4: (Step4_CycIF_measurements_ilastik_v2.m) Makes measurements of signal and foci segmentation 
 % 
 % In order to begin the analysis a set of parameters are needed to be
 % defined by the user 
 % 1) where the files are located and how the names are formatted
-% 2) parameters to choose the size of the field and number of crops per
-% field 
+% 2) parameters to choose the size of the field and number of crops per field
+% 3) parameters for cell segmentation - e.g. cell size, probability thresholds
 
-%%% OUTPUTS FILES AND LOCATIONS 
-% Step 1:
-% FullStacks: AllTheRawData\OmeTifName\FullStacks\OmeTifName_Field_row_column.tif
-% CroppedStacks: AllTheRawData\OmeTifName\CroppedData\OmeTifName_Field_row_column_#ofCrop.tif
-% Coordinates: AllTheRawData\Coordinates_FullStacks\OmeTifName.mat 
-% y= # of pixels in 1 column; x = # of pixels in 1 row ; t = size of field desired 
-% Coordinates Matrix: Coordinates.Field(row,column) = [keep field (0= no, 1=yes), x1, x2, y1, y2]
-% Montage: AllTheRawData\MontageforROI\OmeTifName_montage.tif 
-% Step 3:
-% Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Ilastik_Segmentation\OmeTifName_Field_row_column_Seg.tif
-% Check Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Ilastik_Segmentation\OmeTifName_Field_row_column_checkseg.tif
-% Step 4: 
-% Nucleus & Cytoplasm SegmentedImages: AllTheRawData\ANALYSIS\OmeTifName\Ilastik_Segmentation\OmeTifName_Field_row_column_NucCytSeg.tif
-% Foci Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Foci_Segmentation\OmeTifName_Field_row_column__HSF1_FociSeg.tif
-% Foci Check Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Foci_Segmentation\OmeTifName_Field_row_column__HSF1_FociSeg_check.tif
-% Measurments file: AllTheRawData\ANALYSIS\Analysis_Results\Results_data.mat 
-% Step 5:
-% ROI_pixels: AllTheRawData\ROIpixels\OmeTifName\ROI_pixels.mat: matrix of pixel locations (x,y) of ROIs, vector of row indexes of Centroids that are within the ROI  
-
-
-%%% INPUT FILE LOCATION AND FORMATTING
-%
-% The code will run on the Ilastik Probabilities file and FullStacks file 
-% The expected file structure is that a master folder will contain all of
-% the "ome.tif" data and within that folder, a folder called "ANALYSIS\"
-% will contain all the Ilastik Probabilities and FullStacks data 
 %
 %%% IMPORTANT: Ilastik Output Filename Format for Export Options
 % {dataset_dir}/Ilastik_Probabilities/{nickname}_Probabilities.tif
-
-% eg. AllTheRawData\ANALYSIS\OmeTifName\FullStacks\Ilastik_Probabilities\SlideName_Field_row_column_Probabilities.tif 
-% eg. AllTheRawData\ANALYSIS\OmeTifName\FullStacks\SlideName_Field_row_column.tif 
 
 % ROI:
 % Must be drawn on ImageJ on the the montage created in RUN_Step1_new_fields_preilastik_crops
@@ -62,9 +29,6 @@ clear all
 % 1) THE MASTER OME.TIF DATA FOLDER
 filename = [];
 filename.folders.main = 'Y:\sorger\data\RareCyte\Claire\CCR-002-ImmuneVaccine\';
-% Z:\sorger\
-% Z:\data\IN_Cell_Analyzer_6000\Danae
-% Y:\Danae
 
 % 2) SLIDE SPECIFIC PARAMETERS:
 filename.tissues =  {'MB_1117','MB_1118','MB_1119','MB_1120','MB_1121' ...
@@ -80,7 +44,6 @@ filename.ilastikround = 16; % these are the rounds to be used to segment in Ilas
 
 % 3) USER DESIRED PARAMETERS 
 filename.sizefield = 5000; %Size of field desired for a square 
-filename.ashlarqc.subfield = 10; %make sure filename.sizefield is divisible by this
 filename.crops = 1; %# of cropped fields desired per field 
 options.DAPIbkgd = 1000; % thresh for DAPI used to check if we keep the field
 % options.crops.DAPIbkgd_crops = options.DAPIbkgd*2;
@@ -88,14 +51,14 @@ options.DAPIbkgd = 1000; % thresh for DAPI used to check if we keep the field
 
 % 4) OPTIONS  
 % Step 3: SEGMENTATION OPTIONS 
-options.nuc = 1;    %Channel nucleus Ilastik probability is in
-options.cyt = 2; %Channel cytoplasm Ilastik probability is in
-options.backgr = 3; %Channel background Ilastik probability is in 
-options.cellsize = 15;
-options.bkgdprob_min = 0.2;
-options.cytprob_min  = 0.8;
-options.nucprob_min  = 0.33;
-options.max_prob = 65535; %Maximum Ilastik probability, usually does not change
+options.nuc = 1;        % Channel nucleus Ilastik probability is in
+options.cyt = 2;        % Channel cytoplasm Ilastik probability is in
+options.backgr = 3;     % Channel background Ilastik probability is in 
+options.cellsize = 15;   % Estimated radius of cells in pixels
+options.bkgdprob_min = 0.2;  % Thresh for backgorund Ilastik probability
+options.cytprob_min  = 0.8;  % Thresh for backgorund Ilastik probability
+options.nucprob_min  = 0.33;   % Thresh for backgorund Ilastik probability
+options.max_prob = 65535;     % Maximum Ilastik probability for rescaling
 options.pausetime = 0;
 
 % Step 4: MEASUREMENTS AND FOCI OPTIONS 
@@ -103,7 +66,6 @@ options.date = '20201117';
 
 % Step 5 which slice to use for the montage for ROI
 options.pyramidlevel = 3;
-
 
 % 5) OUTPUT PARAMETERS: DO NOT EDIT 
 filename.folders.ashlared = 'DATA\ashlar\'; 
@@ -126,29 +88,22 @@ filename.folders.results = 'Analysis_Results_20201105\';
 filename.folders.alignqc = 'QC_aligc\';
 filename.folders.ROI = 'ROIs\'; 
 filename.suffix = '.tif';
-% filename.folders.montage = 'MontageforROI\'; 
+filename.folders.montage = 'MontageforROI\'; 
 filename.dim = ['%02d']; %Delimeter 
 filename.overwrite_seg = 0;
 disp('DONE')
+
 %% MAKE SURE TO COMMENT OUT THE OTHER STEPS THAT YOU AREN'T AT YET!!
 % Runs Step 1: (Creating FullStacks, CroppedStacks, Montages) 
-
 Step1_fields_preilastik(filename,options) 
-
+%% clean up the crops to avoid emptry crops in Ilastik training - optional
+% Step2b_filtercrops_v2(filename,options) 
 % after this step need to perform Ilastik step to create probability maps
-
-%% clean up the crops to avoid useless ones - optional
-% Step2b_filtercrops_v2(filename,options)
-%% Runs Step 3: Segmentation
+%% Runs Step 3 (Single cell segmentation)
 Step3_segmentfromilastik_v2_LabelAndDilate(filename, options) 
-%% Runs Step 4: Measurments
+%% Runs Step 4 (Single cell measurments)
 Step4_CycIF_measurements_ilastik(filename, options) 
-% pause(10*60)
-
 %% Extract montages for ROI
 Step5_montageforROI_v2(filename, options)
 
-%% Run Step 5: (ROI Selection)
-
-Step6_AshlarAlignmentQC(filename, options)
 
